c7fa79d377250de2a03336a026e9e785b2d65dad
Vadim Godunko <vgodunko@gmail.com>
2024-06-10 11:52:03 +0400

    Do synchronization with front of the signals and draw them.

--- source/lado-ui.adb
+++ source/lado-ui.adb
@@ -14,11 +14,48 @@ with A0B.ARMv7M.Cache_Utilities;
 with A0B.Types;
 
 with LADO.Acquisition;
+with LADO.Painter;
 
 package body LADO.UI is
 
    procedure PendSV_Handler is null
      with Export, Convention => C, External_Name => "PendSV_Handler";
+
+   procedure Draw_Line
+     (Y     : A0B.Types.Integer_32;
+      Mask  : A0B.Types.Unsigned_16;
+      First : A0B.Types.Unsigned_32);
+
+   ---------------
+   -- Draw_Line --
+   ---------------
+
+   procedure Draw_Line
+     (Y     : A0B.Types.Integer_32;
+      Mask  : A0B.Types.Unsigned_16;
+      First : A0B.Types.Unsigned_32)
+   is
+      use type A0B.Types.Integer_32;
+      use type A0B.Types.Unsigned_16;
+      use type A0B.Types.Unsigned_32;
+
+      Y_Current : A0B.Types.Integer_32  := Y;
+      Current   : A0B.Types.Unsigned_32 := First;
+
+   begin
+      Painter.Set_Color (16#03C0#);
+
+      for X in A0B.Types.Integer_32 (0) .. A0B.Types.Integer_32 (799) loop
+         Y_Current :=
+           Y
+             + (if (LADO.Acquisition.Buffer (Current) and Mask) = 0
+                  then 20 else 0);
+
+         Painter.Fill_Rect (X, Y_Current, 1, 1);
+
+         Current := @ + 1;
+      end loop;
+   end Draw_Line;
 
    ----------------
    -- Initialize --
@@ -37,6 +74,9 @@ package body LADO.UI is
 
    procedure Run is
       use type System.Storage_Elements.Storage_Count;
+      use type A0B.Types.Unsigned_32;
+
+      First : A0B.Types.Unsigned_32;
 
    begin
       Acquisition :
@@ -64,12 +104,26 @@ package body LADO.UI is
             Previous : A0B.Types.Unsigned_16 := LADO.Acquisition.Buffer (0);
 
          begin
-            for Current of LADO.Acquisition.Buffer loop
-               --  exit Acquisition when Previous /= Current;
-               Previous := Current;
+            for Index in LADO.Acquisition.Buffer'Range loop
+               First := Index;
+
+               exit Acquisition
+                 when Previous /= LADO.Acquisition.Buffer (Index);
+
+               Previous := LADO.Acquisition.Buffer (Index);
             end loop;
          end;
       end loop Acquisition;
+
+      First := A0B.Types.Unsigned_32'Max (@, 10);
+      First := @ - 10;
+
+      Draw_Line (40, 2#0000_0001#, First);
+      Draw_Line (80, 2#0000_0010#, First);
+      Draw_Line (120, 2#0000_0100#, First);
+      Draw_Line (160, 2#0000_1000#, First);
+      Draw_Line (200, 2#0001_0000#, First);
+      Draw_Line (240, 2#0010_0000#, First);
 
       raise Program_Error;
       --  loop
